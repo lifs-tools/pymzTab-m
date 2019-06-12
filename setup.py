1e28bce005b6a281b206357827a85296fa670479
Nils Hoffmann <nils.hoffmann@isas.de>
2019-06-12 15:57:19 +0200

    Added travis integration and distribution building.

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 from setuptools import setup
+import setuptools
 
-setup(
-    # ...,
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymztab-m",
+    version="0.0.1",
+    author="Nils Hoffmann",
+    author_email="nils.hoffmann@isas.de",
+    description="The Python3 language bindings for mzTab-M",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lifs-tools/pymzTab-m",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     setup_requires=["pytest-runner"],
-    tests_require=["pytest"],
-    # ...,
+    tests_require=["pytest"]
 )
 
