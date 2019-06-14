9ae0ebf75ad460bb1dc2f16b2811c291efc83686
Nils Hoffmann <nils.hoffmann@isas.de>
2019-06-14 09:53:14 +0200

    Added github edit link

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,3 +55,11 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 master_doc = 'index'
+
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "lifs-tools", # Username
+    "github_repo": "pymzTab-m", # Repo name
+    "github_version": "master", # Version
+    "conf_py_path": "/docs/source/", # Path in the checkout to the docs root
+}
